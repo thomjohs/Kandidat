0fea9c1c84cc933b92a818ae9df7e9d20ebe52c2
Jaxang <42186879+Jaxang@users.noreply.github.com>
2019-03-25 15:54:17 +0100

    Processerat

--- ManipuleraData.py
+++ ManipuleraData.py
@@ -7,8 +7,8 @@ import supp
 standardLen=10
 cutOfIndex=40
 
-in_file = 'PreprocessedData\ArenSwipeNext1.csv'
-out_file = 'ProcessedData\ArenSwipeNext1.csv'
+in_file = 'PreprocessedData\JohanswipeNext1.csv'
+out_file = 'ProcessedData\JohanSwipeNext1.csv'
 
 
 def file_to_framedata(csv_file):
