ab0ec657003a67771bd2319c9ed9b9facdf765f1
Jaxang <42186879+Jaxang@users.noreply.github.com>
2019-03-28 13:10:23 +0100

    Tog bort kommentarer

--- ReadData.py
+++ ReadData.py
@@ -7,27 +7,6 @@ import msvcrt
 # Get the configuration parameters from the configuration file
 #configParameters = radar.parseConfigFile(radar.configFileName)
 
-# START QtAPPfor the plot
-#app = radar.QtGui.QApplication([])
-
-# Set the plot
-# pg.setConfigOption('background','w')
-# win = pg.GraphicsWindow(title="2D scatter plot")
-# p = win.addPlot()
-# p.setXRange(-0.5,0.5)
-# p.setYRange(0,1.5)
-# p.setLabel('left',text = 'Y position (m)')
-# p.setLabel('bottom', text= 'X position (m)')
-# s = p.plot([],[],pen=None,symbol='o')
-
-# vin = pg.GraphicsWindow(title="Velocity scatter plot")
-# q = vin.addPlot()
-# q.setXRange(0,2.0)
-# q.setYRange(-round(configParameters["maxVelocity"]+0.5),round(configParameters["maxVelocity"]+0.5))
-# q.setLabel('left',text = 'V radial vilocity (m/s)')
-# q.setLabel('bottom', text= 'R distance (m)')
-# t = q.plot([],[],pen=None,symbol='o')
-
 # Main loop
 label = 'background'
 detObj = {}
@@ -36,22 +15,10 @@ currentIndex = 0
 j = 0
 while True:
     try:
-
-        # elapsed=time.time()-tic
-        # print("Tid mellan hämtning", elapsed)
-        # tic=time.time()
         # Update the data and check if the data is okay
         dataOk, detObj = radar.update(detObj)
-        # print("after update")
         if dataOk:
-            # elapsed=time.time()-toc
-            # print("Tid för update", elapsed)
-            # toc=time.time()
-            # print("ok main")
-            # Store the current frame into frameData
-            # frameData[currentIndex] = detObj
             if msvcrt.kbhit():
-
                 key = msvcrt.getch()
                 if key == b'1':
                     label = 'slideUp'
@@ -74,6 +41,8 @@ while True:
                     label = 'background'
                 print(f'Current label: {label}')
             detObj['Label'] = label
+
+            # Store the current frame into frameData
             frameData.append(detObj)
             currentIndex += 1
 
