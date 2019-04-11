71be4c4d81117d301f753d6a915734c8a1302d39
Bliipie <31474752+Bliipie@users.noreply.github.com>
2019-04-11 15:28:18 +0200

    art_data tested

--- ML_tester.py
+++ ML_tester.py
@@ -73,7 +73,9 @@ plotFile = f'Plots\\ts{time_steps}bs{batch_size}lstmOut{lstm_output}st{stateful}
 resultFile = "results.csv"
 
 data = supp.shuffle_gestures(ml.load_folder(input_folder))
-art_data = supp.shuffle_gestures(np.concatenate(ml.load_folder(art_folder), ml.load_data("GoodBackground1")))
+
+art_data = supp.shuffle_gestures(np.concatenate
+                                 ([ml.load_folder(art_folder), ml.load_data("GoodBackground1.csv")], axis=0))
 
 
 
@@ -93,8 +95,8 @@ x_train, x_test, y_train, y_test = ml.split_data(data, vector_size, outputs,
 
 art_x, _, art_y, _ = ml.split_data(art_data, vector_size, outputs, 1)
 
-x_train = np.concatenate(x_train, art_x)
-y_train = np.concatenate(y_train, art_y)
+x_train = np.concatenate([x_train, art_x], axis=0)
+y_train = np.concatenate([y_train, art_y], axis=0)
 
 x_train = x_train[:len(x_train) // 1000 * 1000 + time_steps]
 x_test = x_test[:len(x_test) // 1000 * 1000 + time_steps]
@@ -102,8 +104,8 @@ y_train = y_train[:len(y_train) // 1000 * 1000 + time_steps]
 y_test = y_test[:len(y_test) // 1000 * 1000 + time_steps]
 
 
-print(ml.count_gestures(y_test))
 print(ml.count_gestures(y_train))
+print(ml.count_gestures(y_test))
 
 print(f'{len(x_train)}, {len(x_test)}, {len(y_train)}, {len(y_test)}')
 
